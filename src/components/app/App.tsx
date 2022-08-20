6a809403c43aed9c0c405d8ef3404d8627e67e20
Tatiana <ganzhinovat@yandex.ru>
2022-08-20 14:32:25 +0300

    init: add start files

---
+++ src/components/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="headling">
+          Здесь будет наш проект RSLang
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types/TAppTest.ts
@@ -0,0 +1,5 @@
+type Something = {
+  id: string;
+}
+
+export default Something;
