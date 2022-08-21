487163681f352b3b01a13121be6c1d246dbdee98
Tatiana <ganzhinovat@yandex.ru>
2022-08-21 23:38:03 +0300

    add: basic css style, styled-components and ant libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'antd/dist/antd.min.css';
+import './styles/index.css';
 import App from './components/app/App';
 
 const root = ReactDOM.createRoot(
