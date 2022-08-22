d95ddd3dcf5011692332324cb0204b53dcbf8a7d
Aleksei P <civil_a@list.ru>
2022-08-22 19:01:45 +0300

    refactor: rename page link, replace app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { MainPage } from '../pages/main';
-import { TextBookPage } from '../pages/textbook';
-import { SprintPage } from '../pages/sprint';
-import { AudioPage } from '../pages/audioCall';
-import { StatisticPage } from '../pages/statistic';
-import { NotFound } from '../pages/notFound';
+import { MainPage } from './pages/main';
+import { TextBookPage } from './pages/textbook';
+import { SprintPage } from './pages/sprint';
+import { AudioPage } from './pages/audio';
+import { StatisticPage } from './pages/statistic';
+import { NotFound } from './pages/notFound';
 
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
       <Route path='/' element = {<MainPage/>}/>
       <Route path='/textbook' element = {<TextBookPage/>}/>
       <Route path='/sprint' element = {<SprintPage/>}/>
-      <Route path='/audiocall' element = {<AudioPage/>}/>
+      <Route path='/audio' element = {<AudioPage/>}/>
       <Route path='/statistic' element = {<StatisticPage/>}/>
       <Route path='*' element = {<NotFound/>}/>
     </Routes>
