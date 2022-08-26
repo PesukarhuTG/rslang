0cb510e7fe59cbd2ec70439980efa423d1b03f84
Aleksey P <civil_a@list.ru>
2022-08-26 16:17:06 +0300

    refactor: change card's style

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,27 +1,24 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import soundico from '../assets/svg/sound-icon.svg';
+import MemoryCard from '../types/Card';
 import { DBLink } from '../types/DataBaseLink';
 
-interface ICard {
-  id: string;
-  group: number;
-  page: number;
-  word: string;
-  image: string;
-  audio: string;
-  audioMeaning: string;
-  audioExample: string;
-  textMeaning: string;
-  textExample: string;
-  transcription: string;
-  wordTranslate: string;
-  textMeaningTranslate: string;
-  textExampleTranslate: string;
+interface WordExamplesProps {
+  text: string;
+  textTranslate: string;
 }
+const WordExamples: React.FC<WordExamplesProps> = ({ text, textTranslate }) => {
+  return (
+    <WordMeaning>
+      <p dangerouslySetInnerHTML={{ __html: `${text}` }}></p>
+      <p>{textTranslate}</p>
+    </WordMeaning>
+  );
+};
 
 interface WordsProps {
-  word: ICard;
+  word: MemoryCard;
 }
 
 const FlashCard: React.FC<WordsProps> = ({ word }) => {
@@ -39,74 +36,62 @@ const FlashCard: React.FC<WordsProps> = ({ word }) => {
   return (
     <Card>
       <CardImage src={`${DBLink + word.image}`} alt={word.word} />
-      <CardBody>
-        <WordDeclaration>
+      <WordDeclaration>
+        <Word>
           <WordTranscription>
             {word.word} {word.transcription}
           </WordTranscription>
-          <SoundIco onClick={play ? playAudio : () => {}}></SoundIco>
-        </WordDeclaration>
-        <WordTranslate children={word.wordTranslate} />
-      </CardBody>
-      <CardMeaning>
-        <p children={word.textMeaning} />
-        <p>{word.textMeaningTranslate}</p>
-      </CardMeaning>
-      <WordMeaning word={word.word} meaning={word.textMeaning} />
+          <WordAudio onClick={play ? playAudio : () => {}}></WordAudio>
+        </Word>
+        <WordTranslation children={word.wordTranslate} />
+      </WordDeclaration>
+      <WordExamples text={word.textMeaning} textTranslate={word.textMeaningTranslate} />
+      <WordExamples text={word.textExample} textTranslate={word.textExampleTranslate} />
     </Card>
   );
 };
 
-interface WordMeaningProps {
-  word: string;
-  meaning: string;
-}
-const WordMeaning: React.FC<WordMeaningProps> = ({ word, meaning }) => {
-  const meaningArr: string[] = meaning.split(' ');
-  const tagfinder = meaningArr.map(elem =>
-    elem.includes(`>${word}<`) ? React.createElement('i', { children: elem }) : elem
-  );
-  return <>{tagfinder.join(' ')}</>;
-};
-
 const Card = styled.div`
-  max-width: 440px;
+  margin: 0 auto;
+  min-width: 440px;
+  min-height: 890px;
   width: 100%;
-  max-height: 690px;
+  height: fit-content;
   display: flex;
   flex-direction: column;
-  border-radius: 10px;
+  & > :first-child {
+    border-radius: 10px 10px 0 0;
+  }
+  & > :last-child {
+    border-radius: 0 0 10px 10px;
+  }
 `;
 const CardImage = styled.img`
-  border-radius: 10px 10px;
   width: 100%;
-  height: 350px;
-`;
-const CardBody = styled.div`
-  width: 100%;
-  display: flex;
-  background-color: var(--card-background);
-  flex-direction: column;
-  justify-content: space-evenly;
-  height: 130px;
-  padding 0 25px 0 20px;
-  border-bottom 1px solid var(--primary);
+  height: 100%;
 `;
 const WordDeclaration = styled.div`
+  width: 100%;
+  min-height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: var(--card-background);
+  padding 0 25px 0 20px;
+`;
+const Word = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 const WordTranscription = styled.div`
-  font-family: 'Open Sans';
-  font-style: normal;
   font-weight: 700;
   font-size: 36px;
   color: var(--primary);
 `;
 
-const SoundIco = styled.div`
+const WordAudio = styled.div`
   background: url(${soundico}) no-repeat;
   width: 40px;
   height: 40px;
@@ -116,22 +101,24 @@ const SoundIco = styled.div`
   }
 `;
 
-const WordTranslate = styled.div`
-  font-family: 'Open Sans';
-  font-style: normal;
+const WordTranslation = styled.div`
   font-weight: 700;
   font-size: 32px;
   color: var(--primary-light);
 `;
 
-const CardMeaning = styled.div`
+const WordMeaning = styled.div`
   width: 100%;
   display: flex;
+  gap:10px;
   flex-direction: column;
-  justify-content: space-evenly;
-  padding 0 25px 0 20px;
+  padding 15px 25px 25px 20px;
   background-color: var(--card-background);
-  border-radius: 0 0 10px 10px;
+  border-top 1px solid var(--primary);
+
+  & > *{
+    margin: 0px;
+  }
 `;
 
 export default FlashCard;
--- src/pages/TextbookPage.tsx
+++ src/pages/TextbookPage.tsx
@@ -2,48 +2,64 @@ import React from 'react';
 import FlashCard from '../components/FlashCard';
 import Layout from '../components/Layout/Layout';
 
-// type Querry = {
-//   [key: string]: string;
-// };
-
-// const fetchreq = async (url: string, method: 'GET' | 'POST' = 'GET', querry?: Querry) => {
-//   const link = 'https://react-learnwords-example.herokuapp.com';
-//   let querryUrl = '';
-//   if (querry) {
-//     const querryKeys: string[] = Object.keys(querry);
-//     querryUrl = '?' + querryKeys.map(key => `${key}=${querry[key]}`).join('&');
-//   }
-//   const fullUrl = `${link}/${url}${querryUrl}`;
-//   const response = await fetch(fullUrl, {
-//     method: method,
-//   });
-//   return await response.json();
-// };
-
-// const words = fetchreq('words', 'GET', { group: '1', page: '1' });
-
-const exampleCard = {
-  id: '5e9f5ee35eb9e72bc21af733',
-  group: 1,
-  page: 2,
-  word: 'weapon',
-  image: 'files/03_0660.jpg',
-  audio: 'files/03_0660.mp3',
-  audioMeaning: 'files/03_0660_meaning.mp3',
-  audioExample: 'files/03_0660_example.mp3',
-  textMeaning: 'A <i>weapon</i> is an object used to hurt people.',
-  textExample: 'Swords have been used as <b>weapons</b> for thousands of years.',
-  transcription: '[wépən]',
-  textExampleTranslate: 'Мечи использовались в качестве оружия на протяжении тысячелетий',
-  textMeaningTranslate: 'Оружие - это объект, используемый для причинения вреда людям',
-  wordTranslate: 'оружие',
-};
+const exampleCard = [
+  {
+    id: '5e9f5ee35eb9e72bc21af733',
+    group: 1,
+    page: 2,
+    word: 'weapon',
+    image: 'files/03_0660.jpg',
+    audio: 'files/03_0660.mp3',
+    audioMeaning: 'files/03_0660_meaning.mp3',
+    audioExample: 'files/03_0660_example.mp3',
+    textMeaning: 'A <i>weapon</i> is an object used to hurt people.',
+    textExample: 'Swords have been used as <b>weapons</b> for thousands of years.',
+    transcription: '[wépən]',
+    textExampleTranslate: 'Мечи использовались в качестве оружия на протяжении тысячелетий',
+    textMeaningTranslate: 'Оружие - это объект, используемый для причинения вреда людям',
+    wordTranslate: 'оружие',
+  },
+  {
+    id: '5e9f5ee35eb9e72bc21afbfc',
+    group: 3,
+    page: 4,
+    word: 'café',
+    image: 'files/05_1885.jpg',
+    audio: 'files/05_1885.mp3',
+    audioMeaning: 'files/05_1885_meaning.mp3',
+    audioExample: 'files/05_1885_example.mp3',
+    textMeaning: 'A <i>café</i> is a small restaurant where you can buy drinks and simple meals.',
+    textExample: 'Monica works part-time at a <b>café</b>.',
+    transcription: '[kæféi]',
+    textExampleTranslate: 'Моника работает неполный рабочий день в кафе',
+    textMeaningTranslate: 'Кафе - это небольшой ресторан, где можно купить напитки и простые блюда',
+    wordTranslate: 'кафе',
+  },
+  {
+    id: '5e9f5ee35eb9e72bc21afc0b',
+    group: 3,
+    page: 4,
+    word: 'width',
+    image: 'files/05_1900.jpg',
+    audio: 'files/05_1900.mp3',
+    audioMeaning: 'files/05_1900_meaning.mp3',
+    audioExample: 'files/05_1900_example.mp3',
+    textMeaning: '<i>Width</i> is the distance from one side of something to the other side.',
+    textExample: 'The man is measuring the <b>width</b> of the box.',
+    transcription: '[widθ]',
+    textExampleTranslate: 'Человек измеряет ширину коробки',
+    textMeaningTranslate: 'Ширина - это расстояние от одной стороны чего-либо до другой стороны',
+    wordTranslate: 'ширина',
+  },
+];
 
 const TextbookPage = () => {
   return (
     <Layout>
       <p>Учебник</p>
-      <FlashCard word={exampleCard}></FlashCard>
+      <FlashCard word={exampleCard[0]}></FlashCard>
+      <FlashCard word={exampleCard[1]}></FlashCard>
+      <FlashCard word={exampleCard[2]}></FlashCard>
     </Layout>
   );
 };
