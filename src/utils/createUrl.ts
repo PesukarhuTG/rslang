5994b59d4e5c2e39a245010798e079d5b6fbd1f2
Aleksei P <civil_a@list.ru>
2022-08-27 22:59:12 +0300

    fix: prettiers fix

--- src/utils/createUrl.ts
+++ src/utils/createUrl.ts
@@ -1,17 +1,17 @@
 import { DBLink } from '../types/DataBaseTypes';
 
 export interface Options {
-	[key: string]: string | number
-};
+  [key: string]: string | number;
+}
 
 const createUrl = (endpoint: string, options?: Partial<Options>): string => {
-	let querryUrl: string = '';
-	if (options) {
-		const querryKeys: string[] = Object.keys(options);
-		querryUrl = '?' + querryKeys.map(key => `${key}=${options[key]}`).join('&');
-	}
-	const fullUrl: string = `${DBLink}${endpoint}${querryUrl}`;
-	return fullUrl;
+  let querryUrl: string = '';
+  if (options) {
+    const querryKeys: string[] = Object.keys(options);
+    querryUrl = '?' + querryKeys.map(key => `${key}=${options[key]}`).join('&');
+  }
+  const fullUrl: string = `${DBLink}${endpoint}${querryUrl}`;
+  return fullUrl;
 };
 
 export default createUrl;
--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -2,12 +2,12 @@ import CreateUrl from './createUrl';
 import { Options } from './createUrl';
 
 const getData = async (endpoint: string, options?: Options) => {
-	const fullUrl = CreateUrl(endpoint, options);
-	const response: Response = await fetch(fullUrl);
-	if (!response.ok) {
-		throw new Error(`${response.status}  - ${response.statusText}`);
-	} else {
-		return response.json()
-	};
+  const fullUrl = CreateUrl(endpoint, options);
+  const response: Response = await fetch(fullUrl);
+  if (!response.ok) {
+    throw new Error(`${response.status}  - ${response.statusText}`);
+  } else {
+    return response.json();
+  }
 };
 export default getData;
