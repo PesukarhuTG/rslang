b99531d43935f9be3dbeabe739f231f503d27ad2
Tatiana <ganzhinovat@yandex.ru>
2022-08-28 18:50:47 +0300

    fix: init props in sprint game

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -4,16 +4,17 @@ import { Rate as StarsRate } from 'antd';
 
 type Props = {
   count?: number;
+  lifeCount?: number;
 };
 
 /* если назначим условие, что только 3 ошибки допустимы, то изначально ставим 3
-зеленые звезды (count), если ошибка - уменьшаем кол-во окрашенных в зеленый (defaultValue).
-Потом можно добавить проверку, если defaultValue = 0, то игра провалена*/
+зеленые звезды (count), если ошибка - уменьшаем кол-во окрашенных в зеленый (lifeCount).
+Потом можно добавить проверку, если lifeCount = 0, то игра провалена*/
 
-const Stars: React.FC<Props> = ({ count = 3 }) => {
+const Stars: React.FC<Props> = ({ count = 3, lifeCount = 3 }) => {
   return (
     <StatusStars>
-      <StyledStars defaultValue={2} count={count} disabled />
+      <StyledStars count={count} defaultValue={lifeCount} disabled />
     </StatusStars>
   );
 };
