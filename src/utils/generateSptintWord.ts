7c46bc726fe64e97eb10afaad9829d9e8fcde010
Aleksei P <civil_a@list.ru>
2022-08-28 16:03:47 +0300

    create sprint basic functions

---
+++ src/utils/generateSptintWord.ts
@@ -0,0 +1,22 @@
+import MemoryCard from '../types/Card';
+
+const generateSptintWord = (wordsArray: MemoryCard[]) => {
+  const CORRECT_RATE = 0.5;
+  const arrLength = wordsArray.length;
+  const index = Math.floor(arrLength * Math.random());
+  const word = {
+    id: wordsArray[index].id,
+    word: wordsArray[index].word,
+    wordTranslate: wordsArray[index].wordTranslate,
+    correct: true,
+  };
+
+  if (Math.random() < CORRECT_RATE) {
+    const shift = Math.floor(19 * Math.random() + 1);
+    const newIndex = (index + shift) % arrLength;
+    word.wordTranslate = wordsArray[newIndex].wordTranslate;
+    word.correct = false;
+  }
+  return word;
+};
+export default generateSptintWord;
---
+++ src/utils/getSeveralPageData.ts
@@ -0,0 +1,23 @@
+import MemoryCard from '../types/Card';
+import getData from './getData';
+
+async function* generateData(start: number, end: number) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+type Rage = {
+  start: number;
+  end: number;
+};
+const getSeveralPageData = async (group: number, rage: Rage) => {
+  let generator = generateData(rage.start, rage.end);
+  let result: MemoryCard[] = [];
+  for await (let page of generator) {
+    let response = await getData('words', { group, page });
+    result.push(response);
+  }
+  return result.flat();
+};
+
+export default getSeveralPageData;
