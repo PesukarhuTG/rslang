09e9c12ed84ac0d1594f7ab77dce2c2a2a89ccf4
Serjml <Serjml@list.ru>
2022-09-02 20:44:11 +0300

    refactor: move interfaces to the type folder

---
+++ src/types/responses/RegistrationResponse.ts
@@ -0,0 +1,7 @@
+interface RegistrationResponse {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default RegistrationResponse;
--- src/utils/authRequest.ts
+++ src/utils/authRequest.ts
@@ -1,12 +1,8 @@
 import axiosApi from '../http/index';
 import { AxiosResponse } from 'axios';
 import createUrl from './createUrl';
-
-interface RegistrationResponse {
-  name: string;
-  email: string;
-  password: string;
-}
+import RegistrationResponse from '../types/responses/RegistrationResponse';
+import LoginResponse from '../types/responses/LoginResponse';
 
 export const registrationUser = async (
   name: string,
@@ -17,14 +13,6 @@ export const registrationUser = async (
     .post<RegistrationResponse>(createUrl('users'), { name, email, password })
     .then(response => console.log(response.data)) as Promise<AxiosResponse<RegistrationResponse>>;
 };
-
-interface LoginResponse {
-  message: string;
-  token: string;
-  refreshToken: string;
-  userId: string;
-  name: string;
-}
 
 export const loginUser = async (email: string, password: string): Promise<AxiosResponse<LoginResponse>> => {
   return axiosApi.post<LoginResponse>(createUrl('/signin'), { email, password });
